f90d1bbdd590cd2ab5b822ad89e677e1dcb6bd14
GabyyDias <124808915+GabyyDias@users.noreply.github.com>
2023-10-18 09:47:37 -0300

    Delete PrimeiraTela directory

--- PrimeiraTela/src/model/CalcularPerimetro.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class CalcularPerimetro {
-
-	public float CalcularPerimetro(float Perimetro) { 
-		float PerimetroCalculado = 2 * (txtAlturaf + txtBasef); 
-		return PerimetroCalculado; 
-}
-} 
